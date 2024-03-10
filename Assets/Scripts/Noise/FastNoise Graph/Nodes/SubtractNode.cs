5e87dd8acb2b9252948abb9e08e4cb345c7d77ea
carlo697 <carlos.07888@gmail.com>
2024-03-10 17:13:44 -0400

    feat: add subtract node

---
+++ Assets/Scripts/Noise/FastNoise Graph/Nodes/SubtractNode.cs
@@ -0,0 +1,17 @@
+[NodeName("Subtract", "Blends/Subtract")]
+public class SubtractNode : FastNoiseNode {
+  public override string nodeMetadataName => "subtract";
+
+  public float LHS;
+  public float RHS;
+
+  public override FastNoiseInput[] inputs => new FastNoiseInput[] {
+    new FastNoiseHybridInput("LHS", "LHS"),
+    new FastNoiseHybridInput("RHS", "RHS")
+  };
+
+  public override void ApplyValues(FastNoise node) {
+    node.Set("LHS", LHS);
+    node.Set("RHS", RHS);
+  }
+}
