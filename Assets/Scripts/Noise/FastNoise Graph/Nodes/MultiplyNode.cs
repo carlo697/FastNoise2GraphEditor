eb483c4e9d428455e89b816a4866eb87233841f1
carlo697 <carlos.07888@gmail.com>
2024-03-10 17:13:16 -0400

    feat: add multiply node

---
+++ Assets/Scripts/Noise/FastNoise Graph/Nodes/MultiplyNode.cs
@@ -0,0 +1,15 @@
+[NodeName("Multiply", "Blends/Multiply")]
+public class MultiplyNode : FastNoiseNode {
+  public override string nodeMetadataName => "multiply";
+
+  public float RHS;
+
+  public override FastNoiseInput[] inputs => new FastNoiseInput[] {
+    new FastNoiseNodeInput("LHS", true),
+    new FastNoiseHybridInput("RHS", "RHS")
+  };
+
+  public override void ApplyValues(FastNoise node) {
+    node.Set("RHS", RHS);
+  }
+}
