53def3daca796b4bfb46036ba73578a3a6f6a96d
carlo697 <carlos.07888@gmail.com>
2024-03-13 11:15:25 -0400

    feat: add fade node

---
+++ Assets/Scripts/Noise/FastNoise Graph/Nodes/Blends/FadeNode.cs
@@ -0,0 +1,19 @@
+namespace FastNoiseGraph {
+  [NodeName("Fade", "Blends/Fade")]
+  public class FadeNode : FastNoiseNode {
+    public override string nodeMetadataName => "fade";
+
+    public float fade = 0.5f;
+
+    public override FastNoiseInput[] inputs => m_inputs;
+    private FastNoiseInput[] m_inputs = new FastNoiseInput[] {
+      new FastNoiseNodeInput("A", true),
+      new FastNoiseNodeInput("B", true),
+      new FastNoiseHybridInput("Fade", "fade"),
+    };
+
+    public override void ApplyValues(FastNoise node) {
+      node.Set("fade", fade);
+    }
+  }
+}
