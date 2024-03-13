a817e3ffd9406710cbb293048c9d696c8080b241
carlo697 <carlos.07888@gmail.com>
2024-03-13 09:35:11 -0400

    feat: add domain scale node

---
+++ Assets/Scripts/Noise/FastNoise Graph/Nodes/Modifiers/DomainScaleNode.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace FastNoiseGraph {
+  [NodeName("Domain Scale", "Modifiers/Domain Scale")]
+  public class DomainScaleNode : FastNoiseNode {
+    public override string nodeMetadataName => "domainscale";
+
+    public float scale = 1f;
+
+    public override FastNoiseInput[] inputs => m_inputs;
+    private FastNoiseInput[] m_inputs = new FastNoiseInput[] {
+      new FastNoiseNodeInput("Source", true),
+      new FastNoiseSimpleInput("Scale", "scale"),
+    };
+
+    public override void ApplyValues(FastNoise node) {
+      node.Set("scale", scale);
+    }
+  }
+}
